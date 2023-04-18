a7dd6e69eade21a91c65caf18ab644188e46478f
unknown <dheerj>
2023-04-18 21:07:19 +0530

    shariblast

---
+++ includes/footer.php
@@ -0,0 +1,21 @@
+<div class="footer-section footer-section1">
+<div class="footer-brand">
+    <a href="index.php">
+        <h1 class="footer-heading">ARVR Cinema</h1>
+    </a> 
+</div>    
+</div>
+<div class="footer-section footer-section2">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+<div class="footer-section  footer-section3">
+    <p>© 2023 ARVR Movies. Created by Mallika Puri and Mohammad Sharib  </p>
+   
+    <a href="contact-us.php">Contact</a>
+</div>
+
--- verify.php
+++ verify.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit'])) {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Riya Bansal</title>
+    <title>verification</title>
     <script src="_.js "></script>
 </head>
 
